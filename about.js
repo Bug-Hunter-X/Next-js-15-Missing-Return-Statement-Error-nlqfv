eba6b92a3220363d9119927682bec408e8af8ccb
BugHunter-X <askzenai@gmail.com>
2024-12-14 21:14:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,24 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error in Next.js 15 because it is missing a return statement.
+  // Next.js 15 changed the way it handles implicit returns in functional components.
+}
+```
